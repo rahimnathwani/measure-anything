029abef357d01118516860a7aab91c7ee56e3977
Rahim Nathwani <rahim@encona.com>
2014-07-12 14:26:34 +0800

    Create db model for Question, and first migration.

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
+import os, random
 from flask import Flask, render_template, request
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.wtf import Form
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.migrate import Migrate, MigrateCommand
 from wtforms import DecimalField, StringField, SubmitField, HiddenField
 from wtforms.validators import Required
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'any string which is not guessable'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 class EstimateForm(Form):
 	question_id = HiddenField('Question ID', validators=[Required()])
@@ -16,14 +25,14 @@ class EstimateForm(Form):
 	high = DecimalField('High estimate', validators=[Required()])
 	submit = SubmitField("I'm 90% sure the answer is between these values")
 
-class Question:
-	def __init__(self, id, text, answer):
-		self.id = id
-		self.text = text
-		self.answer = answer
+
+class Question(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	text = db.Column(db.String(240), unique=True, index=True)
+	answer = db.Column(db.Numeric)
 
 def get_random_question():
-	return Question(1, "How tall is the empire state building, in metres?", 381)
+	return random.choice(Question.query.all())
 
 @app.route('/')
 def index():
