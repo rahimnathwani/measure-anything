dfb77d0afc3ed8c41ac2c8b15e1dd80b10c4e81f
Rahim Nathwani <rahim@encona.com>
2014-08-09 08:52:03 +0800

    Revert "Settings in a separate file. Simplify by creating app factory. Try but fail to get python-social-auth to work."
    This reverts commit 5cd9c7a04d7644aaf0763565d6e799009e91a188.

--- manage.py
+++ manage.py
@@ -2,22 +2,14 @@ import os
 from flask import Flask
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
-from project import app, db
+from project import create_app, db
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+app = create_app('default')
 manager = Manager(app)
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
-
-'''
-@manager.command
-def syncdb():
-    from flask_example.models import user
-    from social.apps.flask_app import models
-    db.drop_all()
-    db.create_all()
-'''
 
 if __name__ == '__main__':
 	manager.run()
--- project/__init__.py
+++ project/__init__.py
@@ -1,52 +1,27 @@
 import os
-from flask import Flask, g
+from flask import Flask
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext import login
 from flask.ext.migrate import Migrate, MigrateCommand
-from social.apps.flask_app.models import init_social
-from social.apps.flask_app.routes import social_auth
-from social.apps.flask_app.template_filters import backends
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 
-app = Flask(__name__)
+def create_app(config_name):
+    app = Flask(__name__)
 
-app.config.from_object('project.settings')
-    
-bootstrap.init_app(app)
-db.init_app(app)
+#    app.config.from_object(config[config_name])
+#    config[config_name].init_app(app)
+    app.config['SECRET_KEY'] = 'any string which is not guessable'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, '../data.sqlite')
+    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
-from .estimation import estimation as estimation_blueprint
-app.register_blueprint(estimation_blueprint)
+    bootstrap.init_app(app)
+    db.init_app(app)
 
-app.register_blueprint(social_auth)
-init_social(app, db)
+    from .estimation import estimation as estimation_blueprint
+    app.register_blueprint(estimation_blueprint)
 
-from .users import users as users_blueprint
-app.register_blueprint(users_blueprint, url_prefix='/users')
-
-@app.before_request
-def global_user():
-    g.user = login.current_user
-
-@app.teardown_appcontext
-def commit_on_success(error=None):
-    if error is None:
-        db.session.commit()
-
-@app.teardown_request
-def shutdown_session(exception=None):
-    db.session.remove()
-
-@app.context_processor
-def inject_user():
-    try:
-        return {'user': g.user}
-    except AttributeError:
-        return {'user': None}
-
-app.context_processor(backends)
+    return app
