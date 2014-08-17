e645a6564751d07be4c8657eb9617577efbf7832
Rahim Nathwani <rahim@encona.com>
2014-08-17 15:42:29 +0800

    Add send_email function for use by auth system later.

--- project/__init__.py
+++ project/__init__.py
@@ -4,10 +4,12 @@ from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.mail import Mail
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+mail = Mail()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -15,6 +17,7 @@ def create_app(config_name):
 
     bootstrap.init_app(app)
     db.init_app(app)
+    mail.init_app(app)
 
     from .estimation import estimation as estimation_blueprint
     app.register_blueprint(estimation_blueprint)
--- project/settings.py
+++ project/settings.py
@@ -4,8 +4,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SECRET_KEY = 'any string which is not guessable'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '../data.sqlite')
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_SUBJECT_PREFIX = '[Measure Anything]'
 
 try:
-    from localsettings import *
+    from localsettings import SOCIAL_GOOGLE, SOCIAL_FACEBOOK, MAIL_USERNAME, MAIL_PASSWORD
 except:
     print 'localsettings.py file not found.  you should put your oauth 2.0 keys in there'
+
+MAIL_SENDER = 'Measure Anything <%s>' % MAIL_USERNAME
