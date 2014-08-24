ef11a6388dabd07afb3d11f7b097226e68fdf243
Rahim Nathwani <rahim@encona.com>
2014-08-24 18:34:47 +0800

    Add model to keep track of users' estimates.

--- project/estimation/models.py
+++ project/estimation/models.py
@@ -1,6 +1,14 @@
 from .. import db
 
 class Question(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	text = db.Column(db.String(240), unique=True, index=True)
-	answer = db.Column(db.Numeric)
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(240), unique=True, index=True)
+    answer = db.Column(db.Numeric)
+
+class Estimate(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id'))    
+    lowerbound = db.Column(db.Numeric)
+    upperbound = db.Column(db.Numeric)
+    created_on = db.Column(db.DateTime, default=db.func.now())
